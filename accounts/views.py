6bd50a2c1b1141a7ebe4478f3db102898a2e89bc
Carlos Salgado <socd06@gmail.com>
2021-01-31 15:48:16 -0500

    add form to views

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+
+from .forms import PozosForm
+
 # from example
 from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'accounts/home.html')
 
 def reporta(request):
-    return render(request, 'accounts/reporta.html')
+    form = PozosForm()
+    if request.method == 'POST':
+
+        # debugging
+        print(request)
+
+        # save if valid
+        form = PozosForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'accounts/reporta.html', context)
 
 def mapa(request):
     return render(request, 'accounts/mapa.html')
